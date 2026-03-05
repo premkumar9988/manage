00abc3f2680ce2c35456842f608fd1d64f48688f
premkumar9988 <premkumar00786337@gmail.com>
2026-03-05 15:10:38 -0300

    Initial commit - next

--- app1/views.py
+++ app1/views.py
@@ -1,13 +1,22 @@
 from rest_framework import generics
 from .models import members
-from .serializers import MembersSerializer
+from .serializers import MembersSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
 
 class MembersList(generics.ListCreateAPIView):
     queryset = members.objects.all()
     serializer_class = MembersSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class MembersDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = members.objects.all()
     serializer_class = MembersSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
